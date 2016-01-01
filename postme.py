f0fdc400b04227ca73cb3801ccb0576afe009a94
kakitone <kakitone@gmail.com>
2016-01-01 10:31:17 -0800

    Update starter

--- postme.py
+++ postme.py
@@ -1,3 +1,12 @@
+'''
+Typical usage : 
+python postme.py \
+    -o datafolder/ \
+    -a ~/Desktop/allinone/experimentBench/MUMmer3.23/ \
+    -c mFixed2.fasta \
+    -r LR.fasta
+
+'''
 ### Standard librariess
 import argparse
 import json
@@ -11,16 +20,29 @@ import graphLib
 import rankingLib
 import readConnectivityLib
 
-def mainFlow(folderName, mummerLink):
-	print "Welcome to PostMe !!!"
+def mainFlow(folderName, mummerLink, contigsFilename, readsFilename):
+    outputHeader, splitNum, parallelNum, debug= "readToContigHeader",  20, 4, False    
+    dataList = alignmentLib.extractRead2Contig(folderName, mummerLink, readsFilename, contigsFilename, splitNum, outputHeader, parallelNum, debug )
+    connectingReadsList = readConnectivityLib.findConnectingReadsList(dataList)
+    spanReadsList, contigGapReadLookUpDic = readConnectivityLib.findSpanReadsList(connectingReadsList)
+    contigsNamesList = alignmentLib.findContigsNames(folderName, contigsFilename)
+    G = graphLib.formContigGraph(spanReadsList, contigsNamesList)
+    condenseCandidatesList = G.findCondenseCandidatesList()
+    cTestLib.assignCoverageFromDataList(G, dataList,folderName, contigsFilename)
+    scoreList = cTestLib.calculateConfidenceScore(G, condenseCandidatesList)
+    rankingLib.rankAndMerge(folderName,contigsNamesList, contigsFilename, readsFilename, scoreList, contigGapReadLookUpDic)
+
 
 parser = argparse.ArgumentParser(description='PostMe')
 parser.add_argument('-o', '--outputFolder', help= 'Output folder path', required=False)
 parser.add_argument('-a', '--alignmentPath', help= 'MUMmer path', required=False)
+parser.add_argument('-r', '--readsFilename', help= 'Filename for reads file', required=False)
+parser.add_argument('-c', '--contigsFilename', help= 'Filename for contigs file', required=False)
 
 args = vars(parser.parse_args())
 folderName, mummerLink = args['outputFolder'], args['alignmentPath']
+contigsFilename, readsFilename =  args['contigsFilename'], args['readsFilename']
 
 t0 = time.time()
-mainFlow(folderName, mummerLink)
+mainFlow(folderName, mummerLink, contigsFilename, readsFilename)
 print  "Time", time.time() - t0
